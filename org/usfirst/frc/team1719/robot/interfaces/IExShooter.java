5efd731246b227d1e7b3c1c4a23752ed5c46346a
jessica white <white.c.jessica@gmail.com>
2017-01-13 19:50:10 -0500

    Added exshooter subsystem

---
+++ org/usfirst/frc/team1719/robot/interfaces/IExShooter.java
@@ -0,0 +1,6 @@
+package org.usfirst.frc.team1719.robot.interfaces;
+
+public interface IExShooter extends GenericSubsystem{
+	void setSpeed(double speed);
+	double getSpeed();
+}
---
+++ org/usfirst/frc/team1719/robot/subsystems/LogicalExShooter.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team1719.robot.subsystems;
+import org.usfirst.frc.team1719.robot.interfaces.IExShooter;
+
+import edu.wpi.first.wpilibj.SpeedController;
+
+public class LogicalExShooter implements IExShooter {
+
+	private SpeedController shooterMotor;
+	
+	public LogicalExShooter (SpeedController shooterMotor) {
+		this.shooterMotor = shooterMotor;
+	}
+	
+	
+	@Override
+	public void disable() {
+		shooterMotor.set(0);
+	}
+
+	@Override
+	public void setSpeed(double speed) {
+		shooterMotor.set(speed);
+	}
+
+	@Override
+	public double getSpeed() {
+		return shooterMotor.get();
+	}
+
+}
