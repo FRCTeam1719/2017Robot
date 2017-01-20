6ec753da91af9cdc9a25c365a08d07f630de942d
Benny Rubin <bennyrubin@Bennys-MacBook-Air.local>
2017-01-20 17:36:05 -0500

    Added Intake logic and subsystem classes

---
+++ org/usfirst/frc/team1719/robot/interfaces/IIntake.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team1719.robot.interfaces;
+/**
+ * Provides the interface for the intake object
+ * @see <a href="http://104.131.160.86/index.php/Project_Overview#Intake">Intake</a>
+ * @author bennyrubin
+ *
+ */
+public interface IIntake extends GenericSubsystem {
+	void set(double speed);
+	
+	
+}
