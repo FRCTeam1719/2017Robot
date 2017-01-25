ebcb412da4781f3e8ee17a50fe8da0acc9c9f7d6
aaronjeline <aarone@parkschool.net>
2017-01-24 21:59:17 -0500

    Added LimitedRateCommand

---
+++ org/usfirst/frc/team1719/robot/commands/LimitedRateCommand.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc.team1719.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * Abstract Command that can be used to create a command that executes less often than the RIO's polling rate.
+ * @author aaron
+ *
+ */
+public abstract class LimitedRateCommand extends Command{
+	
+	private final int RIO_POLLING_RATE = 100;
+	private final int rate;
+	private int frame = 0;
+	
+	/**
+	 * @param executesPerSecond Desired number of executions per second
+	 */
+	public LimitedRateCommand(int executesPerSecond){
+		rate = executesPerSecond/RIO_POLLING_RATE;
+	}
+	
+	@Override
+	public void initialize(){
+		frame = 0;
+	}
+	
+	@Override
+	public void execute(){
+		if(frame%rate==0){
+			action();
+		}
+		frame++;
+	}
+	
+	/**
+	 * Action you want to happen should be override this
+	 */
+	public abstract void action();
+	
+	
+	
+	
+	
+
+}
