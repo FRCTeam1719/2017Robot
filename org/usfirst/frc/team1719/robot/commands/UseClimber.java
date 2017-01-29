d76ddaa34c6e4f2ae22768f8ae957d1a06b29bbe
Kyle <chickengod37@gmail.com>
2017-01-29 11:15:54 -0500

    Merge w/ master?

---
+++ org/usfirst/frc/team1719/robot/commands/UseClimber.java
@@ -0,0 +1,67 @@
+package org.usfirst.frc.team1719.robot.commands;
+
+import org.usfirst.frc.team1719.robot.interfaces.IClimber;
+import org.usfirst.frc.team1719.robot.interfaces.IMatchTimer;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * command for climbing
+ * @author bennyrubin
+ *
+ */
+public class UseClimber extends Command {
+	
+	public enum states{
+		NOTHING, 
+		PREPPING, 
+		CLIMBINB, 
+		ATTOP;
+	}
+	
+	IClimber climber;
+	IMatchTimer timer;
+	public UseClimber(IClimber climber, IMatchTimer matchTimer){
+		this.climber = climber;
+		timer = matchTimer;
+	}
+	
+	boolean shouldRun;
+	@Override
+	public void initialize(){
+		double time = timer.getMatchTime();
+		if(time<=40)
+			shouldRun = true;
+	}
+	
+	@Override
+	public void execute(){
+		if (shouldRun){
+			climber.setSpeed(1); 
+			//TODO find out how hanging is going to work
+		}
+		
+	}
+	
+	@Override
+	protected boolean isFinished() {
+		
+		return false;
+	}
+	
+	@Override
+	public void end(){
+		climber.disable();
+	}
+	
+	@Override
+	public void interrupted(){
+		climber.disable();
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ org/usfirst/frc/team1719/robot/subsystems/PixySubsys.java
@@ -0,0 +1,56 @@
+package org.usfirst.frc.team1719.robot.subsystems;
+
+import org.usfirst.frc.team1719.robot.interfaces.IPixy;
+import org.usfirst.frc.team1719.robot.interfaces.ISerial;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Subsystem wrapper for PixyLogic
+ * @author Duncan
+ */
+public class PixySubsys extends Subsystem implements IPixy {
+    
+    private class Updater extends Command {
+        private long iter = 0;
+        Updater(PixySubsys subsys) {
+            requires(subsys);
+        }
+        @Override
+        public void execute() {
+            if(iter++ % 10 == 0) {/* Only poll camera at 10Hz = every 10 roboRIO cycles */
+                update();
+            }
+        }
+        @Override
+        protected boolean isFinished() {return false;}
+    }
+    
+    private final PixyLogic logic;
+    
+    public PixySubsys(ISerial serial) {
+        logic = new PixyLogic(serial);
+    }
+    
+    @Override
+    public void disable() {/* No actuators */}
+    
+    @Override
+    public Block[] getBlocks() {
+        return logic.getBlocks();
+    }
+    
+    @Override
+    public void update() {
+        System.out.println("Pixy Update");
+        logic.update();
+    }
+    
+    @Override
+    protected void initDefaultCommand() {
+        /* Update the system every loop */
+        setDefaultCommand(new Updater(this));
+    }
+    
+}
