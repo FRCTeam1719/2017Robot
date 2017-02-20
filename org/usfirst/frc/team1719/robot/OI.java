3d6f47a50332ab9ac170d5de4c5c4c4088f1d615
Kyle <chickengod37@gmail.com>
2017-02-20 14:15:32 -0500

    tuning

--- org/usfirst/frc/team1719/robot/OI.java
+++ org/usfirst/frc/team1719/robot/OI.java
@@ -27,9 +27,9 @@ public class OI implements IOI{
     Joystick driver = new Joystick(0);
     Joystick operator = new Joystick(1);
     
-    JoystickButton revUpButton = new JoystickButton(operator, 4);
-    JoystickButton fireButton = new JoystickButton(operator, 3);
-    JoystickButton runSiloBackwards = new JoystickButton(operator, 5);
+    JoystickButton revUpButton = new JoystickButton(operator, 3);
+    JoystickButton fireButton = new JoystickButton(operator, 1);
+    JoystickButton runSiloBackwards = new JoystickButton(operator, 2);
     
     @Override
     public double getLeftX() {
@@ -114,17 +114,14 @@ public class OI implements IOI{
 	
 	public void init(Robot robot){
 		try {
-			revUpButton.toggleWhenPressed(new ConstantPowerShooter(robot.shooter, robot.getDashboard())); 
-			//revUpButton.toggleWhenPressed(new RevUpShooter(robot.shooter, robot, SmartDashboard.getNumber("Desired RevUpShooter speed (RPS): ", 0))); 
-
-			Button controlShooter = new JoystickButton(operator, 9);
+			//revUpButton.toggleWhenPressed(new ConstantPowerShooter(robot.shooter, robot.getDashboard())); 
+			revUpButton.toggleWhenPressed(new RevUpShooter(robot.shooter, robot, SmartDashboard.getNumber("Desired RevUpShooter speed (RPS): ", 0))); 
 			
-			controlShooter.whileHeld(new UseShooter(robot.shooter, robot));
 			
 			//TODO Decide on button
-			Button intakeToggle = new JoystickButton(operator, 8);
+			Button intakeToggle = new JoystickButton(operator, 4);
 			intakeToggle.toggleWhenPressed(new ToggleIntake(robot.intake));
-			Button unclogIntake = new JoystickButton(operator, 10);
+			Button unclogIntake = new JoystickButton(operator, 5);
 			unclogIntake.whileHeld(new UnclogIntake(robot.intake));
 			Button scanButton = new JoystickButton(driver, 2);
 			scanButton.whenPressed(new PixyScan(robot.pixyMount, new SingleTarget(), robot.pixy, robot.getOI()));
@@ -136,7 +133,7 @@ public class OI implements IOI{
 			
 			
 			runSiloBackwards.whileHeld(new SiloReject(robot.silo));
-			Button runClimber = new JoystickButton(operator, 2);
+			Button runClimber = new JoystickButton(operator, 6);
 			runClimber.whileHeld(new UseClimber(robot.climber,robot.timer));
 			
 			fireButton.whileHeld(new RunSilo(robot.silo, robot.getDashboard()));
